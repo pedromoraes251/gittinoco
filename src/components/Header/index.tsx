662592cc2301ccecb0f8ccd5024908d4a801ffb5
pedromoraes26 <henriquepedro100@outlook.com>
2022-11-08 07:01:33 -0300

    GitTinoco

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+import { Container } from './styles';
+
+import Logo from '../../assets/logo.svg';
+
+interface HeaderProps {
+  size?: 'small' | 'large';
+}
+
+const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
+  <Container size={size}>
+    <header>
+      <img src={Logo} alt="GoFinances" />
+      <nav>
+        <NavLink
+          activeStyle={{
+            paddingBottom: '7px',
+            borderBottom: '3px solid #FF872C',
+          }}
+          exact
+          to="/"
+        >
+          Listagem
+        </NavLink>
+        <NavLink
+          activeStyle={{
+            paddingBottom: '7px',
+            borderBottom: '3px solid #FF872C',
+          }}
+          exact
+          to="/import"
+        >
+          Importar
+        </NavLink>
+      </nav>
+    </header>
+  </Container>
+);
+
+export default Header;
